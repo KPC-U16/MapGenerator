22df12c510b704bd1fc5f2c69f87f999af2e2533
ssuzzukki <yuga.3kan@gmail.com>
2025-04-16 21:59:26 +0900

    make checkの適用

--- src/ImageGenerator.py
+++ src/ImageGenerator.py
@@ -1,14 +1,15 @@
-import re
 import os
+import re
 import sys
-from PIL import Image
 from typing import List
 
+from PIL import Image
 
-#mapファイルのテキストデータを2次元配列に変換するメソッド
-def _change2arr(map_data: str) -> List:
+
+# mapファイルのテキストデータを2次元配列に変換するメソッド
+def changEArray(map_data: List[str]) -> List[List[str]]:
     map_list = []
-    #各行を一マスごとに分割して配列に格納
+    # 各行を一マスごとに分割して配列に格納
     for i in range(len(map_data)):
         if map_data[i] != "" and map_data[i][0] == "D":
             map_list.append(re.split(",|\n", map_data[i].replace("D:", "")))
@@ -23,18 +24,17 @@ def _change2arr(map_data: str) -> List:
     return map_list
 
 
-
-#一つのmapファイルをpngに変換するメソッド
-def _draw_png(file_name: str) -> None:
-    #mapファイルを開いてmap_dataにテキストを格納
-    #print(file_name)
+# 一つのmapファイルをpngに変換するメソッド
+def draWPng(file_name: str) -> None:
+    # mapファイルを開いてmap_dataにテキストを格納
+    # print(file_name)
     file = open("../" + file_name, "r")
     map_data = file.read().split("\n")
     file.close
 
-    map_list = _change2arr(map_data)
+    map_list = changEArray(map_data)
 
-    #アイコン用の画像を開く
+    # アイコン用の画像を開く
     yuka = Image.open("../../icons/yuka.png")
     kabe = Image.open("../../icons/kabe.png")
     item = Image.open("../../icons/item.png")
@@ -43,56 +43,53 @@ def _draw_png(file_name: str) -> None:
 
     icon_size = yuka.width
 
-    #画像の下地を描く
-    size = (len(map_list[0])*icon_size,len(map_list)*icon_size)
-    img = Image.new("RGB", size, (255,255,255))
+    # 画像の下地を描く
+    size = (len(map_list[0]) * icon_size, len(map_list) * icon_size)
+    img = Image.new("RGB", size, (255, 255, 255))
 
-    #各マスのデータに応じて画像を敷き詰める
+    # 各マスのデータに応じて画像を敷き詰める
     for i in range(len(map_list)):
         for j in range(len(map_list[i])):
             if map_list[i][j] == "2":
-                #壁
-                img.paste(kabe, (j*icon_size, i*icon_size))
+                # 壁
+                img.paste(kabe, (j * icon_size, i * icon_size))
             elif map_list[i][j] == "3":
-                #アイテム
-                img.paste(item, (j*icon_size, i*icon_size))
+                # アイテム
+                img.paste(item, (j * icon_size, i * icon_size))
             elif map_list[i][j] == "C":
-                #cool
-                img.paste(cool, (j*icon_size, i*icon_size))
+                # cool
+                img.paste(cool, (j * icon_size, i * icon_size))
             elif map_list[i][j] == "H":
-                #hot
-                img.paste(hot, (j*icon_size, i*icon_size))
+                # hot
+                img.paste(hot, (j * icon_size, i * icon_size))
             else:
-                #それ以外(床)
-                img.paste(yuka, (j*icon_size, i*icon_size))
+                # それ以外(床)
+                img.paste(yuka, (j * icon_size, i * icon_size))
 
-
-
-    #画像を保存
+    # 画像を保存
     img.save(file_name.split(".")[0] + ".png")
-    #img.show("test.png")
+    # img.show("test.png")
 
-    
-#全てのmapファイルをpngに変換するメソッド
-def _draw_all() -> None:
 
+# 全てのmapファイルをpngに変換するメソッド
+def draWAll() -> None:
 
     dir_name = sys.argv[1]
     file_list = os.listdir(dir_name)
 
-    #mapファイルが入ったディレクトリに移動
+    # mapファイルが入ったディレクトリに移動
     os.chdir(dir_name)
 
-    #画像を格納するディレクトリを作成し移動
+    # 画像を格納するディレクトリを作成し移動
     os.mkdir("map_images")
     os.chdir("map_images")
 
     for i in range(len(file_list)):
-        _draw_png(file_list[i])
+        draWPng(file_list[i])
 
-
-    #もとのディレクトリに戻る
+    # もとのディレクトリに戻る
     os.chdir("../../")
     print("完了しました")
 
-_draw_all()
+
+draWAll()
